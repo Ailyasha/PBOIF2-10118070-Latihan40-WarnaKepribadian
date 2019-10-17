07b884f98de48fc0c46bc70ce51f813adbb66b39
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-10-17 23:23:47 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan40/warnakepribadian/User.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan40.warnakepribadian;
+
+/**
+ *
+ * @author Ailyasha
+ * Nama : Aufa Ilyasha
+ * Nim : 10118070
+ * Kelas : IF-2
+ * Nama kelas : Pemrograman Berbasis OO
+ */
+public class User {
+    private String nama, warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    
+}
